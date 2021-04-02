4bc2985b4b53075cef0a85f4e100392749a5638d
topka-going <59656810+topka-going@users.noreply.github.com>
2021-04-02 19:02:17 +0300

    Add files via upload

---
+++ Thumbs/Thumbs/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Thumbs
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer datimer = new DispatcherTimer();
+            datimer.Tick += new EventHandler(UpdateTimer_Tick);
+            datimer.Interval = new TimeSpan(0, 0, 1);
+            datimer.Start();
+        }
+
+        public void UpdateTimer_Tick(object sender,EventArgs e)
+        {
+            DateTime date1 = new DateTime(2021, 11, 24, 6, 0, 0);
+            var date = date1 - DateTime.Now;
+            FooTime.Text = $"{date.Days} дней {date.Hours} часов и {date.Minutes} минут";
+        }
+
+
+
+        
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+
+            Window6 win6 = new Window6();
+            win6.ShowDialog();
+
+        }
+    }
+}
---
+++ Thumbs/Thumbs/Window6.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Thumbs
+{
+    /// <summary>
+    /// Логика взаимодействия для Window6.xaml
+    /// </summary>
+    public partial class Window6 : Window
+    {
+        public Window6()
+        {
+            InitializeComponent();
+
+            DispatcherTimer datimer = new DispatcherTimer();
+            datimer.Tick += new EventHandler(main.UpdateTimer_Tick);
+            datimer.Interval = new TimeSpan(0, 0, 1);
+            datimer.Start();
+            //FooTime6.Text = Convert.ToString(main.UpdateTimer_Tick);
+        }
+            MainWindow main = new MainWindow();
+
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            main.Show();
+        }
+    }
+}
